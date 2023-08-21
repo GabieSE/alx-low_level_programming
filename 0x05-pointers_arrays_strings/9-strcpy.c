e97e536444854a9cb5bab0d48a03dc47209fd4c9
GabieSE <gabrieltuyishimire35@gmail.com>
2023-08-21 09:34:29 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,12 +13,9 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
 	dest[i] = src[i];
 	}
 	dest[i++] = '\0';
+
 	return (dest);
 }
