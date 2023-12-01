47039d28bb8a07f7d9ae939587776f7b99570223
GabieSE <gabrieltuyisimire35@gmaiil.com>
2023-12-01 08:02:43 -0800

    Data structures & Algorithm: Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,31 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
- * @size: size of an array.
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: a pointer to the newly created hash table.
- *         or Null if it fails.
+ * Return: pointer to the hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table;
-unsigned long int i;
+	hash_table_t *h;
+	unsigned long int i;
 
-if (size == 0)
-return (NULL);
-hash_table = malloc(sizeof(hash_table_t));
-if (hash_table == NULL)
-return (NULL);
-hash_table->size = size;
-hash_table->array = malloc(sizeof(hash_node_t *) * size);
-if (hash_table->array == NULL)
-{
-free(hash_table);
-return (NULL);
-}
-for (i = 0; i < size; i++)
-hash_table->array[i] = NULL;
-return (hash_table);
+	h = malloc(sizeof(hash_table_t));
+	if (h == NULL)
+		return (NULL);
+
+	h->size = size;
+	h->array = malloc(sizeof(hash_node_t *) * size);
+	if (h->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		h->array[i] = NULL;
+
+	return (h);
 }
