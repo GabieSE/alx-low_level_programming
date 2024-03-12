3f9ae65bfbc30c3b1291844b9eca64de8be3abd4
GabieSE <gabrieltuyishimire35@gmail.com>
2024-03-12 10:54:02 -0700

    search algos

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -2,38 +2,44 @@
 
 /**
  * interpolation_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return:  first index where value is located or -1 if not found
+ * using an interpolation search algorithm
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t x, l, r;
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t pos;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
 
-	for (l = 0, (r = size - 1); r >= l;)
+	while ((array[high] != array[low]) &&
+	       (value >= array[low]) && (value <= array[high]))
 	{
-		x = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		if (x < size)
-		{
-			printf("Value checked array [%ld] = [%d]\n", x, array[x]);
-		}
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			    * (value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
 		else
-		{
-			printf("Value checked array [%ld] is out of range\n", x);
-			break;
-		}
-
-		if (array[x] == value)
-			return (x);
-		if (array[x] > value)
-			r = x - 1;
-		else
-			l = x + 1;
+			return (pos);
 	}
+	if (value == array[low])
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		return (low);
+	}
+	pos = low + (((double)(high - low) / (array[high] - array[low]))
+		     * (value - array[low]));
+	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
-
 }
